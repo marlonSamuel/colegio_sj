717d7d348f0c92950fc9cc24a2762cbc805eb471
malonSamuel <marlongf2195@gmail.com>
2021-01-21 16:39:59 -0600

    build p

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,6 +13,6 @@
 <body class="hold-transition skin-blue sidebar-mini">
     <div style="height: 100%" id="app"></div>
     <!-- built files will be auto injected -->
-<script type="text/javascript" src="bundle/js/manifest.6aa67b56925988dfee92.js"></script><script type="text/javascript" src="bundle/js/app.da17f49346edcdb89e91.js"></script></body>
+<script type="text/javascript" src="bundle/js/manifest.6aa67b56925988dfee92.js"></script><script type="text/javascript" src="bundle/js/app.b76685cb510e9148639f.js"></script></body>
 
 </html>
