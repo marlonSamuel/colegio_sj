9e04ada5c893efedfadb23dddbb73aed5a84e368
malonSamuel <marlongf2195@gmail.com>
2021-02-04 09:34:59 -0600

    build scopes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,6 +13,6 @@
 <body class="hold-transition skin-blue sidebar-mini">
     <div style="height: 100%" id="app"></div>
     <!-- built files will be auto injected -->
-<script type="text/javascript" src="bundle/js/manifest.6aa67b56925988dfee92.js"></script><script type="text/javascript" src="bundle/js/app.bf5774b262bb15777ece.js"></script></body>
+<script type="text/javascript" src="bundle/js/manifest.6aa67b56925988dfee92.js"></script><script type="text/javascript" src="bundle/js/app.adb415d29bfe49e5a18d.js"></script></body>
 
 </html>
